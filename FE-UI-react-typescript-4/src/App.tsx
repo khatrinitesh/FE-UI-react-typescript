096f34e1e9a2f18d060dbbdc04c0c942929a03f9
nitesh <nitesh.khatri88@gmail.com>
2025-08-08 18:05:49 +0530

    u[dated

---
+++ FE-UI-react-typescript-4/src/App.tsx
@@ -0,0 +1,19 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="content gridGap_10">
+        <h2 className="headTitle">HeadTitle</h2>
+        <h3 className="subTitle">SubTitle</h3>
+        <p className="desc">
+          Cupidatat cupidatat magna ad qui sint nulla dolore cillum consequat
+          Lorem.
+        </p>
+        <button class="responsive-button">Click Me</button>
+      </div>
+    </>
+  );
+}
+
+export default App;
